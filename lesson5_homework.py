3b14e70e26d6cb2c6b34f6ceef166e33079b1dc9
Alex Paladin <alex_paladin@mail.ru>
2020-05-12 18:49:07 +0300

    Large verification added

--- lesson5_homework.py
+++ lesson5_homework.py
@@ -1,4 +1,4 @@
-# please advise how to active the commented rows
+# large verification added
 
 class Gadget:
     def __init__(self, name, color):
@@ -31,22 +31,21 @@ class Tablet(Gadget):
     def __init__(self, name, color, display):
         super().__init__(name, color)
         self.display = display
-        #self.quality = ''
-        #if self.display > 10:
-        #    self.quality = 'large'
+        self.quality = ''
+        if self.display > 10:
+            self.quality = 'LARGE'
 
     def show_name(self):
         a = 'My name is '+self.name+' and I am a tablet PC!'
         print(a)
 
     def describe_yourself(self):
-        print(f'I am {self.color} and I have {self.display} inch display')
-        #print(f'I am {self.color} and I have {self.display} {self.quality} inch display')
+        print(f'I am {self.color} and I have {self.display} {self.quality} inch display')
 
 object_list = [
-            Smartphone('iPhone ','white',64),
-            Smartphone('iPhone','black',128),
-            Tablet('iPad','silver',10.5),
+            Smartphone('iPhone ', 'white', 64),
+            Smartphone('iPhone', 'black', 128),
+            Tablet('iPad', 'silver', 9),
             Tablet('iPad', 'white', 12.9),
             Tablet('Samsung Galaxy Tab', 'white', 10.5)
 ]
